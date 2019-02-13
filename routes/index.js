45e1a4413d4b6cb526d254e685d4efe7c28fe4f1
wilsonwu <iwilsonwu@gmail.com>
2019-02-13 12:05:01 +0800

    add graphql hello world

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
 var router = express.Router();
 
+var schemaHello = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var rootHello = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.post('/hello', graphqlHTTP({
+  schema: schemaHello,
+  rootValue: rootHello,
+  graphiql: true,
+}));
 
 module.exports = router;
